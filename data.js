3b059fb1cf7835bbdb31b92d041992dc6b466ed0
Valber Carneirio <valbercarneiro@outlook.com>
2024-10-10 02:12:22 -0300

    store

---
+++ data.js
@@ -0,0 +1,91 @@
+// Dados dos quadros
+const boards = [
+  {
+    name: "Quadro Natureza",
+    img: "https://via.placeholder.com/200",
+    category: "nature",
+  },
+  {
+    name: "Quadro Urbano",
+    img: "https://via.placeholder.com/200",
+    category: "urban",
+  },
+  {
+    name: "Quadro Universo",
+    img: "https://via.placeholder.com/200",
+    category: "universe",
+  },
+  {
+    name: "Quadro Abstrato",
+    img: "https://via.placeholder.com/200",
+    category: "abstract",
+  },
+  {
+    name: "Quadro Natureza 2",
+    img: "https://via.placeholder.com/200",
+    category: "nature",
+  },
+  {
+    name: "Quadro Arte Digital",
+    img: "https://via.placeholder.com/200",
+    category: "digital",
+  },
+  {
+    name: "Quadro Minimalista",
+    img: "https://via.placeholder.com/200",
+    category: "minimal",
+  },
+];
+
+// Categorias dos quadros com título e descrição
+const categories = [
+  {
+    name: "all",
+    icon: "bi bi-house",
+    title: "Quadros Decorativos para Todos os Gostos",
+    description:
+      "Explore nossa coleção completa de quadros, pensados para transformar qualquer ambiente com arte e estilo.",
+  },
+  {
+    name: "nature",
+    icon: "bi bi-tree",
+    title: "Quadros de Natureza",
+    description:
+      "Traga a beleza da natureza para dentro de sua casa com quadros que capturam a essência de paisagens naturais e vegetações exuberantes.",
+  },
+  {
+    name: "urban",
+    icon: "bi bi-buildings",
+    title: "Arte Urbana em Quadro",
+    description:
+      "Adicione um toque moderno e urbano ao seu ambiente com quadros inspirados em paisagens e cenários urbanos dinâmicos.",
+  },
+  {
+    name: "universe",
+    icon: "bi bi-stars",
+    title: "Quadros do Universo",
+    description:
+      "Descubra o mistério e a imensidão do cosmos com nossos quadros que exploram a beleza do universo e das estrelas.",
+  },
+  {
+    name: "abstract",
+    icon: "bi bi-palette",
+    title: "Arte Abstrata Exclusiva",
+    description:
+      "Quadros abstratos para quem busca arte moderna e interpretações únicas, perfeitos para um toque sofisticado e intrigante.",
+  },
+  {
+    name: "digital",
+    icon: "bi bi-pc",
+    title: "Arte Digital e Criativa",
+    description:
+      "Explore o universo da arte digital com quadros vibrantes e inovadores, trazendo modernidade e originalidade ao seu espaço.",
+  },
+  {
+    name: "minimal",
+    icon: "bi bi-square",
+    title: "Design Minimalista em Quadros",
+    description:
+      "Menos é mais! Quadros minimalistas que valorizam a simplicidade e o design limpo, perfeitos para ambientes elegantes e discretos.",
+  },
+];
